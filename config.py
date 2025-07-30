b7d38e6bda5ec07b15cf37412ec4d1769bba482c
FansterInc <romanrabov26@gmail.com>
2025-07-30 21:03:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8242713185:AAHbwdYBdj_mh5VI25jqkQdhz2cWq4r1yl8")
 TIKTOK_USERNAME = os.getenv("TIKTOK_USERNAME", "fansterinc")
-OWNER_CHAT_ID = int(os.getenv("OWNER_CHAT_ID", "123456789"))
+OWNER_CHAT_ID = int(os.getenv("OWNER_CHAT_ID", "1161990101"))
